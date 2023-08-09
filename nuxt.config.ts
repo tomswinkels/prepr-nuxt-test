51ad391780870785cbe69bbf2c0face90699a1cf
Tom Swinkels <tom@prepr.io>
2023-08-09 09:26:33 +0200

    Remove apollo for test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,11 @@
 
 import { defineNuxtConfig } from 'nuxt/config'
 
-export default defineNuxtConfig({
-  modules: ['@nuxtjs/apollo'],
-  apollo: {
-    clients: {
-      prepr: './apollo/prepr.ts',
-    },
-  },
-})
+// export default defineNuxtConfig({
+//   modules: ['@nuxtjs/apollo'],
+//   apollo: {
+//     clients: {
+//       prepr: './apollo/prepr.ts',
+//     },
+//   },
+// })
